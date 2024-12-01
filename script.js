a8a1a7703c3d053278f53f18a52604fbe7b69247
linagocevska <167706670+linagocevska@users.noreply.github.com>
2024-12-01 18:39:04 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('yesBtn').addEventListener('click', function(){
+    alert('Thank you for your positive response!');
+});
+document.getElemetnById('noBtn').addEventListener('click', function(){ 
+    alert('We appreciate your feedback!')
+});
